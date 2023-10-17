e8f7bca3c91a9f440c33b72fde626b39d29a93d1
Kronan1 <simonlindberg1992@gmail.com>
2023-10-17 16:44:33 +0200

    Add project files.

---
+++ Practice3/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Practice3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> colors = new List<string>()
+            {
+                "Röd",
+                "Grön",
+                "Blå",
+                "Gul",
+                "Lila",
+                "Beige",
+                "Rosa",
+                "Guld"
+            };
+
+
+            // Om strängen "Röd" finns i listan colors skriv ut "Röd finns!"
+            // Skriv annars ut "Röd finns inte."
+
+
+
+
+            // Om strängen "röd" finns i listan colors skriv ut "röd finns!"
+            // Skriv annars ut "röd finns inte."
+
+
+            string[] someColors =
+            {
+                "Svart",
+                "Grå",
+                "Gul",
+                "Brun",
+                "Guld"
+            };
+
+
+            // Loopa igenom arrayen someColors, om färgen i someColors finns i listan colors. Skriv ut "färgen 'färg' finns!"
+            // om färgen inte finns i listan colors skriv "'färgen' finns inte."
+            // Så här ska det se ut om blå finns i listan
+            // färgen Blå finns!
+
+
+
+            
+            // Ändra färgen Blå till Orange utan att veta vilket index färgen har i listan
+            // Skriv sen ut listan
+
+ 
+        }
+    }
+}
